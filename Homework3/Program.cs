2e6e5ed0e6d5defe0f37723a7f2f09f9fe188aa8
Denis <ivdenvas@gmail.com>
2022-11-14 11:10:30 +0300

    finish homework3

--- Homework3/Program.cs
+++ Homework3/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+string userInput1 = Console.ReadLine() ?? "";
+int number1 = int.Parse(userInput1);
+if (number1 % 2 == 0)
+{
+  Console.Write($"{number1}, -> Да"); 
+}
+else 
+{
+  Console.Write($"{number1} -> Нет");
+}
