9640743ab7b7382e87960182072f8ed9fd094371
Denis <ivdenvas@gmail.com>
2022-11-14 14:45:02 +0300

    finish homework4

--- Homework4/Program.cs
+++ Homework4/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+string userInput1 = Console.ReadLine() ?? "";
+int number1 = int.Parse(userInput1);
+int count = 0;
+int evenNumber = 0;
+Console.Write($"{number1} -> ");
+while (count <= number1)
+{
+    evenNumber = count;
+    if (evenNumber % 2 == 0 && evenNumber > 0 && evenNumber < number1)
+    {
+        Console.Write(evenNumber + ", ");
+    }
+    else if (evenNumber == number1 )
+    {
+        Console.Write(evenNumber);
+    }
+     count ++;
+}
